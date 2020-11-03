02d459eff1419124ae903c3d2a439c0eb1099b66
Levi Bostian <levi.bostian@gmail.com>
2020-11-03 15:29:29 -0600

    ci: setup github actions

--- bin/dev_setup.sh
+++ bin/dev_setup.sh
@@ -10,11 +10,7 @@ set -e
 . bin/require.sh
 . bin/db_setup.sh
 
-OUTPUT="/tmp/app_dev.log"
-
 SKAFFOLD_COMMAND=$1
-
-cp /dev/null $OUTPUT
 
 require_kubectl
 require_minikube
@@ -35,14 +31,11 @@ cat << EOF
 EOF
 
 logSuccess "Note: Server will be available at localhost:5000 once it is up!"
-logVerbose "Full script output located at $OUTPUT. In case of an error, check out that file."
 
 ask_to_clear_db
 start_dbs
 
 # Now that our databases are up and ready for connections, let's startup our application. 
 logVerbose "Starting up development server..."
-npm run build >> $OUTPUT 2>&1 # we need to build once to make sure there is a dist/ directory to copy into the Dockerfile
-# Create secrets. We need to create secrets now before our application begins. 
-#kubectl create secret generic app-config --from-file=app/config/projects.json --dry-run=client -o yaml | kubectl apply -f - >> $OUTPUT 2>&1
+npm run build # we need to build once to make sure there is a dist/ directory to copy into the Dockerfile
 K8S_NAMESPACE=default PROJECTS=$(cat app/config/projects.json | base64) DOTENV=$(cat app/.env | base64) skaffold $SKAFFOLD_COMMAND --port-forward 
